5ec53b5201ff399f1ef94982cd770d9ad5bff5f2
sd0623 <deegutlasathwika@gmail.com>
2023-08-10 23:32:42 -0400

    projects - wip

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,9 +3,9 @@ import { ProjectCard } from "./ProjectCard";
 import Carousel from "react-multi-carousel";
 import colorSharp from "../assets/img/color-sharp2.png";
 
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+
+import aws_logo from "../assets/img/aws_logo.png";
+import research from "../assets/img/research.png";
 
 
 export const Projects = () => {
@@ -41,20 +41,21 @@ export const Projects = () => {
                         <h2>Projects</h2>
                         <Carousel responsive={responsive} infinite={true} className="skill-slider">
                                 <div className="item">
-                                    <img src={projImg1} alt="Image" />
+                                    <img src={aws_logo} alt="logo"></img>
                                     <h5>MyMenu</h5>
-                                    <p>Full Stack Web development using AWS amplify, AWS ElasticSearch, AWS AppSync, AWS Cognito, Lambda functions, DynamoDB, and S3 buckets for backend and React and Bootstrap for frontend. </p>
+                                    <p>
+                                        Full Stack Web development using AWS amplify, AWS ElasticSearch, AWS AppSync, AWS Cognito, Lambda functions, DynamoDB, and S3 buckets for backend and React and Bootstrap for frontend. 
+                                        Seamless front-end integration allows users to search and sort their favorite restuarants' menu by their preferences and allergies.
+                                    </p>
                                 </div>
                                 <div className="item">
-                                    <img src={projImg1} alt="Image" />
+                                    <img src={research} alt="logo" className="invert"></img>
                                     <h5>Research Study</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={projImg1} alt="Image" />
                                     <h5>Deep Learning Neural Networks</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={projImg1} alt="Image" />
                                     <h5>Kernel Modules</h5>
                                 </div>
                             </Carousel>
