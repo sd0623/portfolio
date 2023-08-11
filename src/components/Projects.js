5059aa7cac6e88ba39d97fe14fe026951a72c1ec
sd0623 <deegutlasathwika@gmail.com>
2023-08-11 00:26:57 -0400

    projects - wip2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,9 @@ import colorSharp from "../assets/img/color-sharp2.png";
 
 import aws_logo from "../assets/img/aws_logo.png";
 import research from "../assets/img/research.png";
+import budgie from "../assets/img/budgie.png";
+import nn_logo from "../assets/img/nn_logo.png";
+import linux from "../assets/img/linux.png";
 
 
 export const Projects = () => {
@@ -42,7 +45,7 @@ export const Projects = () => {
                         <Carousel responsive={responsive} infinite={true} className="skill-slider">
                                 <div className="item">
                                     <img src={aws_logo} alt="logo"></img>
-                                    <h5>MyMenu</h5>
+                                    <h5 href="#">MyMenu</h5>
                                     <p>
                                         Full Stack Web development using AWS amplify, AWS ElasticSearch, AWS AppSync, AWS Cognito, Lambda functions, DynamoDB, and S3 buckets for backend and React and Bootstrap for frontend. 
                                         Seamless front-end integration allows users to search and sort their favorite restuarants' menu by their preferences and allergies.
@@ -50,13 +53,34 @@ export const Projects = () => {
                                 </div>
                                 <div className="item">
                                     <img src={research} alt="logo" className="invert"></img>
-                                    <h5>Research Study</h5>
+                                    <h5 href="#">Research Study</h5>
+                                    <p>
+                                        Conducted and evaluated machine learning algorithms - logistic regression, random forest, and support vector machine - for predictions of coronary artery disease.
+                                        Used kaggle open-source dataset and trained models to identify the most accurate and robust predictions model.
+                                    </p>
                                 </div>
                                 <div className="item">
-                                    <h5>Deep Learning Neural Networks</h5>
+                                    <img src={budgie} alt="logo"></img>
+                                    <h5 href="#">Budgie</h5>
+                                    <p>
+                                        Collaborated in team project to develop a full-stack application for budget tracker using Firebase, Express, React, and Node with REST API.
+                                        Focused mainly on the frontend development of the website using React.js and Tailwind CSS and mobile application using Flutter Dart.
+                                    </p>
                                 </div>
                                 <div className="item">
-                                    <h5>Kernel Modules</h5>
+                                    <img src={nn_logo} alt="logo"></img>
+                                    <h5 href="#">Image Processing</h5>
+                                    <p>
+                                    Trained image recognization on the dataset of raccoon, squirrels, and wombat animals using deep learning environment in Anaconda.
+                                    Tweaked the convolution neural network parameters for a better accuracy of image classification.
+                                    </p>
+                                </div>
+                                <div className="item">
+                                    <img src={linux} alt="logo" className="invert"></img>
+                                    <h5 href="#">Linux Kernel</h5>
+                                    <p>
+
+                                    </p>
                                 </div>
                             </Carousel>
                         </div>
